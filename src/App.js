1bda19be12262fd564ef977200550c454469b9cc
allibrdoine <allibrodine81@gmail.com>
2022-10-08 15:52:11 -0500

    About section created, photo added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="">
-      <header className="">
-        
+    <div>
+      <header>
+        <h1>Alli Brodine</h1>
       </header>
+
+      <main>
+        <About></About>
+      </main>
+  
+      <footer>
+        <a href="https://www.facebook.com/alli.brodine/" target="blank">Facebook</a>
+        <a href="https://www.instagram.com/allibrodine/?hl=en" target="blank">Instagram</a>
+        <a href="https://www.linkedin.com/in/alli-brodine-8826a6224/" target="blank">LinkedIn</a>
+      </footer>
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BeachPhoto from '../../assets/images/OntheBeach.jpg';
+
+//create About section
+function About() {
+    return(
+        <section>
+            <h2 id="about">About Me</h2>
+            <img src={BeachPhoto} style={{ height: 100, width: 100 }} alt="Alli on the beach" />
+            <p>I am just getting started on my journey as a web developer. Learning is in progress but I am 
+                getting better every day. I have a good eye for design so I hope to focus my career on front 
+                end development to start and then see where that takes me.</p>
+        </section>
+    );
+}
+
+export default About;
