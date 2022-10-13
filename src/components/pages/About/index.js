126ccb6d8949bc6cafdb0f251c0d9d0820afb00c
allibrdoine <allibrodine81@gmail.com>
2022-10-13 15:32:13 -0500

    add more bio text

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -19,7 +19,17 @@ function About() {
             <div className="about-text">
                 <p>I am just getting started on my journey as a web developer. Learning is in progress but I am 
                     getting better every day. I have a good eye for design so I hope to focus my career on front 
-                    end development to start and see where that takes me.</p>
+                    end development to start and see where that takes me.
+                    
+                    <br/>
+                    I began my coding adventure just a short time ago, when I enrolled in the Full Stack Coding Bootcamp 
+                    at SMU in Dallas. It has been a short but extremely intense journey that I have managed to excell in 
+                    while maintaining a full time job. It has been no easy feat but I have come out at the top of my class
+                    and now I can't wait to make a much needed career change and continue on my journey in web development.
+                    <br/>
+                    My passions lie in food, music, gardening and art. I have always enjoyed the creative aspects of design
+                    but also greatly appreciate logic and structure and, this is why I believe web development is an appropriate 
+                    field for me.</p>
             </div>
         </section>
     );
